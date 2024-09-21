283d575da7ca0b50afadb741305165d1acd56442
Lemon Hall <lemonhall@gmail.com>
2024-09-21 16:53:43 +0800

    读取日志的试做

--- control.gd
+++ control.gd
@@ -1,9 +1,26 @@
 extends Control
 
 var database : SQLite
+var ItemListResult : Array
 
 func _on_ready() -> void:
 	database = SQLite.new()
 	database.path = "res://app.db"
 	database.open_db()
+	ItemListResult = database.select_rows("notes","id>560",["id","title"])
+	print(ItemListResult)
+	for item in ItemListResult:
+		$ItemList.add_item(item["title"])
+	pass # Replace with function body.
+
+
+func _on_item_list_item_selected(index: int) -> void:
+	print("选择了第几个？："+str(index))
+	print(ItemListResult[index])
+	var db_id = str(ItemListResult[index]["id"])
+	var content = database.select_rows("notes","id="+db_id,["content"])
+	var real_content = content[0]["content"]
+	print(real_content)
+	$CodeEdit.clear()
+	$CodeEdit.text = real_content
 	pass # Replace with function body.
